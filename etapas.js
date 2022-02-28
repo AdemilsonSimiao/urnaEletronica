48981ba2c059cabd4567c9745192a8749ba22da6
unknown <pc@DESKTOP-3IVL2JC.none>
2022-02-28 18:03:41 -0300

    teste

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[{
+            numero: '38111',
+            nome: 'Fulano',
+            partido: 'ABC',
+            fotos:[{
+                url: '38111.jpeg', legenda: 'Vereador'
+            }]
+        },
+        {
+            numero: '77222',
+            nome: 'Beltrano',
+            partido: 'DEFG',
+            fotos:[{
+                url: '77222.jpeg', legenda: 'Vereador'
+            }]
+        },
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        candidatos:[{
+            numero: '99',
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos:[
+                {url: '99.jpeg', legenda: 'Prefeito'},
+                {url: '98.jpeg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+            numero: '84',
+            nome: 'Zulano',
+            partido: 'DEFG',
+            vice: 'Zul',
+            fotos:[
+                {url: '84.jpeg', legenda: 'Prefeito'},
+                {url: '85.jpeg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        ]
+    }
+];
+console.log(etapas)
